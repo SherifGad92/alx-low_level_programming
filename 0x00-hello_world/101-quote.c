322efb360fd61408aebf1649a2c985ed1cb83d0b
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-13 19:05:35 -0700

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,16 +3,15 @@
 /**
  * main - Entry point
  *
- * Discription - C programm that writes a quote
- * ssize_t write(int fd. const void *buf. size_t cout);
+ * Discription: print a quote using write function
+ * ssize_t write(int fd. const void *buf. size_t count);
  *
- * Return: Always 1 (error)
-*/
+ * Return: 1 (error)
+ */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(1. quo, 59);
-		return (1);
+	write(1. quo, 59);
+	return (1);
 }
