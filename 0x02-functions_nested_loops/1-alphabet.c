9a0a0f06ef790e41cf09ce58c0a3a295ae283907
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-18 17:57:46 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Discription: prints alphabet in lower case.
- *
+ * print_alphabet - utilizes on the _putchar function to print
+ *			the alphabet a -z
  */
 
 void print_alphabet(void)
