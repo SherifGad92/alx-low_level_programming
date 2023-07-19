86fbc05c756f7f3cbb79dd57ffa26fae2e4e41a2
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-18 19:44:17 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,17 @@ void times_table(void)
 		_putchar(48);
 	{
 		for (mult = 1; mult <= 9; mult++)
-	{
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
+		{
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((prod / 10) + 48);
-		_putchar((prod % 10) + 48);
-	}
-	_putchar('\n');
+
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
+		}
+		_putchar('\n');
 	}
 }
