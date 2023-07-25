a67b28a6c3b6e2e85b8ab76dfb6c16a2f6c1de7e
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-24 17:36:15 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - Finds the length of a string.
+ *
+ * @s: input string
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; *s != '\0'; ++count)
+
+	return (count);
+}
