96333b11f47f194537650cb34bf7391eba08f9b9
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-24 18:38:03 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: input string
+ *
+ * Return
+ */
+
+void rev_string(char *s)
+{
+	int i, x;
+	char t;
+
+	while (s[i])
+		++i;
+	for (x = 0; x < i / 2; x++)
+	{
+		t = s[x];
+		s[x] = s[i - 1 - x];
+		s[i - 1 - x] = t;
+	}
+}
