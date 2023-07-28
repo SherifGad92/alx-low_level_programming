dee0805a7004f8749731822e671bfd6ba63f78f5
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-07-27 18:38:56 -0700

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,16 +14,16 @@ char *leet(char *s)
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		while (s[x] == 'a' || s[x] == 'A')
+			s[x] = '4';
+		while (s[x] == 'e' || s[x] == 'E')
+			s[x] = '3';
+		while (s[x] == 'o' || s[x] == 'O')
+			s[x] = '0';
+		while (s[x] == 't' || s[x] == 'T')
+			s[x] = '7';
+		while (s[x] == 'l' || s[x] == 'L')
+			s[x] = '1';
 	}
 	return (s);
 }
