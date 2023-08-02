693075807710254f553aa31dab82b339423080b9
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-08-02 15:01:22 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse
+ *
+ * @s: string pointer
+ *
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar ('\n');
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
