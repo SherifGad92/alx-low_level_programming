641becc93d44e3b1324b77664b69f6780b66f4b4
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-08-10 14:31:13 -0700

    task0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,18 +2,17 @@
 
 /**
  * malloc_checked - allocates memory using malloc
+ *
  * @b: size
  *
  * Return:  pointer to the allocated memory (success), exit(98) (fail)
  */
+
 void *malloc_checked(unsigned int b)
 {
 	int *bl = malloc(b);
 
 	if (bl == 0)
-	{
 		exit(98);
-	}
-
 	return (bl);
 }
