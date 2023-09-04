ebb3b3e160fd0bb2a94cda2af409b0354b2c23c4
SherifGad92 <sherif.abdelhamid13@gmail.com>
2023-09-04 15:17:20 -0700

    tasks

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text file and prints it to POSIX standard output
+ *
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed (sucess)
+ * returns 0 (fail)
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
+}
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename: filename.
+ * @text_content: content writed in the file.
+ *
+ * Return: 1 (success). -1 (fails).
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int nlet;
+	int rwr;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (let = 0; text_content[let]; let++)
+		;
+
+	rwr = write(fd, text_content, let);
+
+	if (rwr == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
